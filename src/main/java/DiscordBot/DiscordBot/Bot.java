6fcbf9d7f91cb4b4ef7c71691a60d7c8c6ffed4d
jupadhyay1 <jupadhyay1@yahoo.com>
2021-11-06 13:01:40 -0700

    Small changes

--- src/main/java/DiscordBot/DiscordBot/Bot.java
+++ src/main/java/DiscordBot/DiscordBot/Bot.java
@@ -60,6 +60,11 @@ public class Bot extends ListenerAdapter
     			tc.sendMessage("!hello bot- Yubaba says hi to a bot!").queue();
     			tc.sendMessage("!hello human- Yubaba says hi to a human!").queue();
     		}
+    		else if(args[0].equalsIgnoreCase("gif"))
+    		{
+
+    			event.getMember().modifyNickname("YUBABA").queue();
+    		}
 
     		
     	}
