cca9f6dc9cae651e2c36d5de78f57d36b3fb0bf7
Hyunwook Yoo <hyunwook5636@gmail.com>
2021-11-06 10:36:37 -0700

    make bot to take message

--- src/main/java/DiscordBot/DiscordBot/Bot.java
+++ src/main/java/DiscordBot/DiscordBot/Bot.java
@@ -4,14 +4,18 @@ import javax.security.auth.login.LoginException;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.ReadyEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class Bot extends ListenerAdapter 
 {
     public static void main( String[] args ) throws LoginException, InterruptedException
     {
-    	JDA jda = JDABuilder.createDefault("OTA2NTcxMzE0NjIwNDI4MzQ5.YYakVA.RLppb6Wh5XhFw6_JJQQj-acpXPA")
+    	JDA jda = JDABuilder.createDefault("OTA2NTcxMzE0NjIwNDI4MzQ5.YYakVA.Rl59i7XERTTJ__ATxX5RqURgkLE")
     			.addEventListeners(new Bot())
     			.build();
     	jda.awaitReady();
@@ -22,4 +26,24 @@ public class Bot extends ListenerAdapter
     	System.out.println(event.getJDA().getToken());
     }
     
+    public void onMessageReceived(MessageReceivedEvent event) {
+    	User user = event.getAuthor();
+    	TextChannel tc = event.getTextChannel();
+    	Message msg = event.getMessage();
+    	if (user.isBot()) return;
+    	if (msg.getContentRaw().charAt(0) == '!') {
+    		String[] args = msg.getContentRaw().substring(1).split(" ");
+    		if (args.length <= 0) return;
+    		if (args[0].equalsIgnoreCase("test")) {
+    			tc.sendMessage("Hello, " + user.getAsMention()).queue();
+    		} else if (args[0].equalsIgnoreCase("hello")) {
+    			if (args.length < 2) return;
+    			if (args[1].equalsIgnoreCase("bot")) {
+    				tc.sendMessage("Hello, Sir!").queue();
+    			} else if (args[1].equalsIgnoreCase("human")) {
+    				tc.sendMessage("I'M NOT HUMAN!").queue();
+    			}
+    		}
+    	}
+    }
 }
