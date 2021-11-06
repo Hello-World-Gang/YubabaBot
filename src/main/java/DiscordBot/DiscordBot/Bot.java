79b2d29db199e1960abea05d63ce4f632011cc4c
Haruki Miyazaki <83580034+HarukiMiya@users.noreply.github.com>
2021-11-07 05:41:46 +0900

    Update Bot.java

--- src/main/java/DiscordBot/DiscordBot/Bot.java
+++ src/main/java/DiscordBot/DiscordBot/Bot.java
@@ -1,5 +1,8 @@
 package DiscordBot.DiscordBot;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import javax.security.auth.login.LoginException;
 
 import net.dv8tion.jda.api.JDA;
@@ -26,24 +29,19 @@ public class Bot extends ListenerAdapter
     	System.out.println(event.getJDA().getToken());
     }
     
+    public String getNickName(User user) {
+    	String nickname = user.getAsMention().toString();
+        return nickname.substring(0, 1);
+    }
+    
     public void onMessageReceived(MessageReceivedEvent event) {
     	User user = event.getAuthor();
     	TextChannel tc = event.getTextChannel();
     	Message msg = event.getMessage();
     	if (user.isBot()) return;
-    	if (msg.getContentRaw().charAt(0) == '!') {
-    		String[] args = msg.getContentRaw().substring(1).split(" ");
-    		if (args.length <= 0) return;
-    		if (args[0].equalsIgnoreCase("test")) {
-    			tc.sendMessage("Hello, " + user.getAsMention()).queue();
-    		} else if (args[0].equalsIgnoreCase("hello")) {
-    			if (args.length < 2) return;
-    			if (args[1].equalsIgnoreCase("bot")) {
-    				tc.sendMessage("Hello, Sir!").queue();
-    			} else if (args[1].equalsIgnoreCase("human")) {
-    				tc.sendMessage("I'M NOT HUMAN!").queue();
-    			}
-    		}
-    	}
+        if( "summonYubaba".equals(msg.getContentRaw())){
+            tc.sendMessage("konnnichiwa, " + user.getAsMention()).queue();
+            event.getMember().modifyNickname(user.getName().substring(0,1)).queue();
+        }
     }
 }
